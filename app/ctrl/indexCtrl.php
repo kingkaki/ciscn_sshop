db1b7e9e532bc21c42f441df20bd6cff7a73437d
kingkk <917536172@qq.com>
2018-05-18 09:38:44 +0800

    一些整理

--- app/ctrl/indexCtrl.php
+++ app/ctrl/indexCtrl.php
@@ -3,7 +3,6 @@ namespace app\ctrl;
 
 class indexCtrl extends \core\mypro
 {
-    //显示所有留言
     public function index()
     {
         jump('/shop/');
--- app/ctrl/shopCtrl.php
+++ app/ctrl/shopCtrl.php
@@ -16,27 +16,27 @@ class shopCtrl extends \core\mypro
 		$this->display('shop.html');
 	}
 
-	public function test()
-	{
-		$data['name'] = $this->generate(6);
-		$data['desc'] = $this->generate(20);
-		$data['amount'] = rand(20,1000);
-		$data['price'] = rand(10,500);
-		$model = new commoditysModel();
-		$model->addOne($data);
-	}
+	// public function test()  //加商品用的。。
+	// {
+	// 	$data['name'] = $this->generate(6);
+	// 	$data['desc'] = $this->generate(20);
+	// 	$data['amount'] = rand(20,1000);
+	// 	$data['price'] = rand(10,500);
+	// 	$model = new commoditysModel();
+	// 	$model->addOne($data);
+	// }
 
 
-	private function generate( $length = 8 ) { 
-	// 密码字符集，可任意添加你需要的字符 
-	$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; 
-	$password = ''; 
-	for ( $i = 0; $i < $length; $i++ ) 
-	{ 
-		$password .= $chars[ mt_rand(0, strlen($chars) - 1) ]; 
-	} 
-		return $password; 
-	} 
+	// private function generate( $length = 8 ) { 
+	// // 密码字符集，可任意添加你需要的字符 
+	// $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; 
+	// $password = ''; 
+	// for ( $i = 0; $i < $length; $i++ ) 
+	// { 
+	// 	$password .= $chars[ mt_rand(0, strlen($chars) - 1) ]; 
+	// } 
+	// 	return $password; 
+	// } 
 
 
 }
